c0331c0b530170c53bd15dfe652241d7aed8621a
Chandrakanth <kanthchandra@users.noreply.github.com>
2017-07-06 18:33:42 +0530

    remove certain migrations

--- models/migrations.sql
+++ models/migrations.sql
@@ -5,23 +5,5 @@ do $$
 
     insert into "questions" ("question", "optionA", "optionB", "optionC", "optionD", "answer", "createdAt", "updatedAt") values ('When did India get its independence?', '1956', '1947', '1958', '1959', 'B', '2016-06-01', '2016-06-01');
 
-    insert into "questions" ("question", "optionA", "optionB", "optionC", "optionD", "answer", "createdAt", "updatedAt") values ('Who is the CEO of Postman labs?', 'Abhinav Asthana', 'Abhijit Kane', 'Shamasis Bhattacharya', 'Ankit Sobti', 'A', '2016-06-01', '2016-06-01');
-
-    insert into "questions" ("question", "optionA", "optionB", "optionC", "optionD", "answer", "createdAt", "updatedAt") values ('When was Postmanlabs founded?', '2014', '2013', '2015', '2014', 'D', '2016-06-01', '2016-06-01');
-
-    insert into "questions" ("question", "optionA", "optionB", "optionC", "optionD", "answer", "createdAt", "updatedAt") values ('Who is the CTO of Postmanlabs', 'Abhinav Asthana', 'Abhijit Kane', 'Shamasis Bhattacharya', 'Ankit Sobti', 'D', '2016-06-01', '2016-06-01');
-
-    insert into "questions" ("question", "optionA", "optionB", "optionC", "optionD", "answer", "createdAt", "updatedAt") values ('What is the most kickass feature postman?', 'newman', 'postman collections', 'chrome interceptor', 'cli extensions', 'B', '2016-06-01', '2016-06-01');
-
-    insert into "questions" ("question", "optionA", "optionB", "optionC", "optionD", "answer", "createdAt", "updatedAt") values ('Who is the CHO of postmanlabs?', 'Carl', 'Hooper', 'Cooper', 'Bradley', 'C', '2016-06-01', '2016-06-01');
-
-    insert into "questions" ("question", "optionA", "optionB", "optionC", "optionD", "answer", "createdAt", "updatedAt") values ('Who is the present PM of India?', 'AB Vajpayee', 'H D Deve Gowda', 'Narendra Modi', 'Manmohan Singh', 'C', '2016-06-01', '2016-06-01');
-
-    insert into "questions" ("question", "optionA", "optionB", "optionC", "optionD", "answer", "createdAt", "updatedAt") values ('What is new tax system that came into existence from July 1 2017?', 'VAT', 'CAT', 'CST', 'GST', 'D', '2016-06-01', '2016-06-01');
-
-    insert into "questions" ("question", "optionA", "optionB", "optionC", "optionD", "answer", "createdAt", "updatedAt") values ('Who is the current finance minister of India?', 'AB Vajpayee', 'H D Deve Gowda', 'Narendra Modi', 'Arun Jaitley', 'D', '2016-06-01', '2016-06-01');
-
-    insert into "questions" ("question", "optionA", "optionB", "optionC", "optionD", "answer", "createdAt", "updatedAt") values ('Which is the national language of India?', 'Hindi', 'Marathi', 'Kannada', 'India doesnt have a national language', 'D', '2016-06-01', '2016-06-01');
-
   end
 $$;
